13779248748ed0dd8945066d105cf2142cbcb050
Grant-S-Clark <grantsamuelclark@gmail.com>
2022-08-23 09:36:55 -0400

    no msg

---
+++ printversion/main.cpp
@@ -0,0 +1,12 @@
+//Name: Grant Clark
+//File: main.cpp
+
+#include <iostream>
+#include <QtCore>
+
+int main()
+{
+    std::cout << "Qt Version: " << qVersion() << std::endl;
+    
+    return 0;
+}
